446939a66703bd27e59f72ea3560e25e87476d72
evan <evan@wpi.edu>
2017-05-26 02:53:07 -0400

    Add neatline captivity project

---
+++ captivity/themes/neatlight/javascripts/show.js
@@ -0,0 +1,25 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-NeatLight
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+$(function() {
+
+  var content = $('div.exhibit, div.narrative');
+
+  var position = function() {
+    content.css('height', $(window).height());
+    Neatline.execute('MAP:updateSize');
+  };
+
+  $(window).resize(function() { position(); });
+  position();
+
+});
+
+window.location.pathname = window.location.pathname.replace(/\/show\//, "/fullscreen/")
